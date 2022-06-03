8f77fc375336e99a6520d11172a607bf8e6ba25c
Imesha Udara <43387957+udnadee@users.noreply.github.com>
2022-06-03 15:19:14 +0530

    Update MissingValue.js

--- MissingValue.js
+++ MissingValue.js
@@ -9,27 +9,27 @@ function findMissVal( ar ){
         tot += i;
 
         if(min > i){
-            min = i
+            min = i;
         }
         else if(max < i){
-            max = i
+            max = i;
         }
     })
 
     let OriginalArray =[] ;
     
     for (let i = min; i <= max; i++) {
-        OriginalArray.push(i)
+        OriginalArray.push(i);
     }
     
     let b = OriginalArray.length;
 
     if(a+1 == b){
-        let missing = OriginalArray.filter(e=>!ar.includes(e))
-        console.log("missing value: ",missing)
+        let missing = OriginalArray.filter(e=>!ar.includes(e));
+        console.log("missing value: ",missing);
     }
     else{
-        console.log("missing value is either first value:",min-1," or last value:", max+1)
+        console.log("missing value is either first value:",min-1," or last value:", max+1);
     }
 
     
